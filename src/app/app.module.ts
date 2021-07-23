a828fed43a0227ee53cfce3c487461d741338bed
DlopezS98 <01dlopezs98@gmail.com>
2021-07-23 13:50:31 -0600

    administration panel - first advances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SignUpComponent } from './authentication/sign-up/sign-up.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadCrumbsComponent } from './shared/components/bread-crumbs/bread-crumbs.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    SignUpComponent,
+    DashboardComponent,
+    BreadCrumbsComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
